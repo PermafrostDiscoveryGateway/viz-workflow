a9cfb14f07d216af1042bd2a89024c0185e360bb
Matt Jones <gitcode@magisa.org>
2022-04-07 16:54:58 -0800

    Basic workflow showing parsl execution on k8s.
    See issue #1.

---
+++ viz-workflow.py
@@ -0,0 +1,39 @@
+# Example script for executing parsl workflow apps on kubernetes
+
+import datetime
+import parsl
+from parsl import python_app
+
+def main():
+    '''Main program to execute all stats.'''
+
+    parsl.set_stream_logger()
+    #from parslexec import local_exec
+    from parslexec import htex_kube
+    #parsl.load(local_exec)
+    parsl.load(htex_kube)
+
+    size = 5
+    stat_results = []
+    for year in range(size):
+        for lat in range(size):
+            current_time = datetime.datetime.now()
+            print(f'Schedule job at {current_time} for {year} and {lat}')
+            stat_results.append(calc_stat_lat(year, lat))
+    stats = [r.result() for r in stat_results]
+    print(sum(stats))
+
+@python_app
+def calc_stat_lat(year, lat):
+    import datetime
+    import time
+    current_time = datetime.datetime.now()
+    print(f'Starting job at {current_time} for {year} and {lat}')
+    prod = year*lat
+    time.sleep(5)
+    return(prod)
+
+
+if __name__ == "__main__":
+    main()
+
