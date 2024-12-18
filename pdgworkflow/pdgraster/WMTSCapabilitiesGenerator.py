8e78e3c6a6ad0a6148ec03bf2d380d2e5a8e8470
Alona Kosobokova <alonakosobokova@Alonas-MacBook-Pro.local>
2024-12-18 13:19:46 -0800

    WMTSCapabilitiesGenerator.py: removed redundut parameters, added encoding=utf-8

---
+++ pdgworkflow/pdgraster/WMTSCapabilitiesGenerator.py
@@ -0,0 +1,247 @@
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+from typing import Dict
+
+class WMTSCapabilitiesGenerator:
+    """
+    A class to generate WMTS Capabilities XML for a given dataset.
+
+    Usage Example:
+        generator = WMTSCapabilitiesGenerator(
+            title="PDG Ice-wedge polygon high",
+            base_url="https://arcticdata.io/data/tiles",
+            doi="10.18739/A2KW57K57",
+            layer_title="iwp_high",
+            layer_identifier="iwp_high",
+            bounding_box={
+                "left": -179.91531896747117,
+                "right": 179.91531896747247,
+                "bottom": 50.16996707215903,
+                "top": 80.0978646943821
+            },
+            tile_format="image/png",
+            tile_matrix_set="WGS1984Quad",
+            tile_width=256,
+            tile_height=256,
+            max_z_level=15
+        )
+        xml_str = generator.generate_capabilities()
+    """
+      # Class-level constants for namespaces and schema
+    XMLNS = {
+        "default": "http://www.opengis.net/wmts/1.0",
+        "ows": "http://www.opengis.net/ows/1.1",
+        "xlink": "http://www.w3.org/1999/xlink",
+        "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        "gml": "http://www.opengis.net/gml"
+    }
+    SCHEMA_LOCATION = (
+        "http://www.opengis.net/wmts/1.0 "
+        "http://schemas.opengis.net/wmts/1.0/wmtsGetCapabilities_response.xsd"
+    )
+
+    # Mapping of tile_format to file extension
+    EXTENSION_MAPPING: Dict[str, str] = {
+        "image/png": "png",
+        "image/jpeg": "jpg",
+        "image/jpg": "jpg",
+        "image/tiff": "tiff",
+        "application/vnd.google-earth.kmz+xml": "kmz",
+        "application/vnd.google-earth.kmz+xml;image_type=image/jpeg": "kmz",
+        "application/vnd.google-earth.kmz+xml;image_type=image/png": "kmz",
+        "application/x-esri-shape": "shp",
+        "application/json": "json",
+        "image/tiff;depth=8": "tiff",
+        "image/tiff;depth=16": "tiff",
+        "image/tiff;depth=32f": "tiff"
+    }
+
+    def __init__(
+        self,
+        title: str,
+        base_url: str,
+        doi: str,
+        layer_title: str,
+        layer_identifier: str,
+        bounding_box: Dict[str, float],
+        tile_format: str,
+        tile_matrix_set: str,
+        tile_width: int,
+        tile_height: int,
+        max_z_level: int
+    ):
+        
+        """
+        The following parameters shoudl be read and passed from the config.json file
+        """
+        self.title = title
+        self.base_url = base_url
+        self.doi = doi
+        self.layer_title = layer_title
+        self.layer_identifier = layer_identifier
+        self.bounding_box = bounding_box
+        self.tile_format = tile_format
+        self.tile_matrix_set = tile_matrix_set
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.max_z_level = max_z_level
+
+
+        self.capabilities_url = f"{base_url}/{doi}/WMTSCapabilities.xml"
+        self.tiles_url = f"{base_url}/{doi}/"
+        self.scale_denominator_base = 279541132.0143589
+
+        # Configure resource template based on tile format
+        self.resource_template = self._configure_resource_template()
+
+        self.top_left_corner = "-180 90"
+        self.supported_crs = "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
+        self.well_known_scale_set = "http://www.opengis.net/def/wkss/OGC/1.0/GoogleCRS84Quad"
+
+
+
+    def generate_capabilities(self) -> str:
+        """
+        Generates the WMTS Capabilities XML as a formatted string.
+        
+        Returns:
+            A pretty-printed XML string representing the WMTS Capabilities document.
+        """
+        root = ET.Element("Capabilities", attrib={
+            "xmlns": self.XMLNS["default"],
+            "xmlns:ows": self.XMLNS["ows"],
+            "xmlns:xlink": self.XMLNS["xlink"],
+            "xmlns:xsi": self.XMLNS["xsi"],
+            "xmlns:gml": self.XMLNS["gml"],
+            "xsi:schemaLocation": self.SCHEMA_LOCATION,
+            "version": "1.0.0"
+        })
+
+        self._add_service_identification(root)
+        self._add_operations_metadata(root)
+        self._add_contents(root)
+
+        ET.SubElement(root, "ServiceMetadataURL", attrib={"xlink:href": self.capabilities_url})
+
+        # Pretty-print the XML
+        xml_bytes = ET.tostring(root, encoding="utf-8")
+        parsed_xml = minidom.parseString(xml_bytes)
+
+        # Return the XML string with UTF-8 encoding
+        return parsed_xml.toprettyxml(indent="  ", encoding="UTF-8").decode("utf-8")
+    
+    def _configure_resource_template(self) -> str:
+        """
+        Configure the resource template based on the tile format.
+
+        Returns:
+            A string template for the resource URL.
+
+        Raises:
+            ValueError: If the tile format is unsupported.
+        """
+        inferred_extension = self.EXTENSION_MAPPING.get(self.tile_format)
+        if inferred_extension is None:
+            raise ValueError(f"Unsupported tile format: {self.tile_format}")
+
+        return f"{self.base_url}/{self.doi}/{{TileMatrixSet}}/{{TileMatrix}}/{{TileCol}}/{{TileRow}}.{inferred_extension}"
+
+
+    def _add_service_identification(self, root):
+        service_identification = ET.SubElement(root, "ows:ServiceIdentification")
+        ET.SubElement(service_identification, "ows:Title").text = self.title
+        ET.SubElement(service_identification, "ows:ServiceType").text = "OGC WMTS"
+        ET.SubElement(service_identification, "ows:ServiceTypeVersion").text = "1.0.0"
+
+    def _add_operations_metadata(self, root):
+        operations_metadata = ET.SubElement(root, "ows:OperationsMetadata")
+        self._add_operation(operations_metadata, "GetCapabilities", self.capabilities_url)
+        self._add_operation(operations_metadata, "GetTile", self.tiles_url)
+
+    def _add_operation(self, parent, name, href):
+        operation = ET.SubElement(parent, "ows:Operation", attrib={"name": name})
+        dcp = ET.SubElement(operation, "ows:DCP")
+        http = ET.SubElement(dcp, "ows:HTTP")
+        get = ET.SubElement(http, "ows:Get", attrib={"xlink:href": href})
+        constraint = ET.SubElement(get, "ows:Constraint", attrib={"name": "GetEncoding"})
+        allowed_values = ET.SubElement(constraint, "ows:AllowedValues")
+        ET.SubElement(allowed_values, "ows:Value").text = "RESTful"
+
+    def _add_contents(self, root):
+        contents = ET.SubElement(root, "Contents")
+        layer = ET.SubElement(contents, "Layer")
+        ET.SubElement(layer, "ows:Title").text = "iwp_high"
+        ET.SubElement(layer, "ows:Identifier").text = "iwp_high"
+
+        wgs84_bbox = ET.SubElement(layer, "ows:WGS84BoundingBox")
+        ET.SubElement(wgs84_bbox, "ows:LowerCorner").text = f"{self.bounding_box['left']} {self.bounding_box['bottom']}"
+        ET.SubElement(wgs84_bbox, "ows:UpperCorner").text = f"{self.bounding_box['right']} {self.bounding_box['top']}"
+
+
+        style = ET.SubElement(layer, "Style", attrib={"isDefault": "true"})
+        ET.SubElement(style, "ows:Title").text = "Default Style"
+        ET.SubElement(style, "ows:Identifier").text = "default"
+
+        ET.SubElement(layer, "Format").text = "image/png"
+        tile_matrix_set_link = ET.SubElement(layer, "TileMatrixSetLink")
+        ET.SubElement(tile_matrix_set_link, "TileMatrixSet").text = "WGS1984Quad"
+
+        
+
+        resource_url = ET.SubElement(layer, "ResourceURL", attrib={
+            "format": self.tile_format,
+            "resourceType": "tile",
+            "template": self.resource_template
+        })
+
+        self._add_tile_matrix_set(contents)
+
+    def _add_tile_matrix_set(self, contents):
+        tile_matrix_set = ET.SubElement(contents, "TileMatrixSet", attrib={"xml:id": "WorldCRS84Quad"})
+        ET.SubElement(tile_matrix_set, "ows:Title").text = "CRS84 for the World"
+        ET.SubElement(tile_matrix_set, "ows:Identifier").text = "WGS1984Quad"
+
+        bounding_box = ET.SubElement(tile_matrix_set, "ows:BoundingBox", attrib={"crs": "http://www.opengis.net/def/crs/OGC/1.3/CRS84"})
+        ET.SubElement(bounding_box, "ows:LowerCorner").text = "-180 -90"
+        ET.SubElement(bounding_box, "ows:UpperCorner").text = "180 90"
+
+        ET.SubElement(tile_matrix_set, "ows:SupportedCRS").text = "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
+        ET.SubElement(tile_matrix_set, "WellKnownScaleSet").text = "http://www.opengis.net/def/wkss/OGC/1.0/GoogleCRS84Quad"
+
+        for i in range(self.max_z_level + 1):  # Generate levels from 0 to max_z_level
+            tile_matrix = ET.SubElement(tile_matrix_set, "TileMatrix")
+            ET.SubElement(tile_matrix, "ows:Identifier").text = str(i)
+            ET.SubElement(tile_matrix, "ScaleDenominator").text = str(self.scale_denominator_base / (2 ** i))
+            ET.SubElement(tile_matrix, "TopLeftCorner").text = self.top_left_corner
+            ET.SubElement(tile_matrix, "TileWidth").text = str(self.tile_width)
+            ET.SubElement(tile_matrix, "TileHeight").text = str(self.tile_height)
+            ET.SubElement(tile_matrix, "MatrixWidth").text = str(2 ** (i+1))
+            ET.SubElement(tile_matrix, "MatrixHeight").text = str(2 ** i)
+
+# For testing purposes. Remove after
+if __name__ == "__main__":
+    # Example usage
+    generator = WMTSCapabilitiesGenerator(
+        title="PDG Ice-wedge polygon high",
+        base_url="https://arcticdata.io/data/tiles",
+        doi="10.18739/A2KW57K57",
+        layer_title="iwp_high",
+        layer_identifier="iwp_high",
+        bounding_box={
+            "left": -179.91531896747117,
+            "right": 179.91531896747247,
+            "bottom": 50.16996707215903,
+            "top": 80.0978646943821
+        },
+        tile_format="image/png",
+        tile_matrix_set="WGS1984Quad",
+        tile_width=256,
+        tile_height=256,
+        max_z_level=15
+    )
+    wmts_xml = generator.generate_capabilities()
+
+    # Write the output to a file
+    with open("WMTSCapabilities.xml", "w", encoding="utf-8") as f:
+        f.write(wmts_xml)
+    print("WMTS Capabilities document generated successfully!")
